3655426725685fe04cdfdbb7764d402ffdef3e7f
gr4yx01 <hoyx0101@gmail.com>
2025-04-08 16:07:31 +0100

    app init

---
+++ AppleInvite.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppleInvite = () => {
+  return (
+    <View>
+      <Text>AppleInvite</Text>
+    </View>
+  )
+}
+
+export default AppleInvite
